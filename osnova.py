3dc85497c250f8cc40671bfc865ab7f0defa9f6c
pinyaev-kirill@ya.ru <9853102172Qq>
2020-09-19 12:19:09 +0300

    многозадачность

--- osnova.py
+++ osnova.py
@@ -6,6 +6,7 @@ bot = telebot.TeleBot('1163114593:AAFGXPuRouF8Sx-kOh0nkFU3sWSFieuYnO4')
 name = ''
 surname = ''
 clas = ''
+slov = {}
 
 
 def main(use_logging, level_name):
@@ -17,43 +18,59 @@ def main(use_logging, level_name):
 @bot.message_handler(commands=['start'])
 def start_message(mess):
     global name
-    sent = bot.send_message(mess.from_user.id, 'Привет, Как тебя зовут?')
+    sent = bot.send_message(mess.chat.id, 'Привет, Как тебя зовут?')
+    print(mess.chat.id)
+    slov[mess.chat.id] = ['start']
+    print(slov)
     bot.register_next_step_handler(sent, surname_message)
 
 
 @bot.message_handler(commands=['help'])
 def help_message(mess):
-    bot.send_message(mess.from_user.id, '/start')
+    bot.send_message(mess.chat.id, '/start')
 
 
 def surname_message(message):
     global name
-    name = message.text
-    sent = bot.send_message(message.chat.id, f'{name.capitalize()} как ваша фамилия?')
-    bot.register_next_step_handler(sent, class_message)
+    if len(slov[message.chat.id]) > 0:
+        slov[message.chat.id].append(message.text)
+        sent = bot.send_message(message.chat.id, f'{slov[message.chat.id][1].capitalize()} как ваша фамилия?')
+        print(slov)
+        bot.register_next_step_handler(sent, class_message)
 
 
 def class_message(message):
     global name, surname
-    surname = message.text
-    sent = bot.send_message(message.from_user.id,
-                            f'{surname.capitalize()} {name.capitalize()} в каком вы классе'
-                            f'(написать через нижние подчеркивание, пример 10_1)')
-    bot.register_next_step_handler(sent, end_message)
+    if len(slov[message.chat.id]) > 1:
+        slov[message.chat.id].append(message.text)
+        sent = bot.send_message(message.chat.id,
+                                f'{slov[message.chat.id][2].capitalize()}'
+                                f' {slov[message.chat.id][1].capitalize()} в каком вы классе'
+                                f'(написать через нижние подчеркивание, пример 10_1)')
+        print(slov)
+        bot.register_next_step_handler(sent, end_message)
 
 
 def end_message(message):
     global clas, name, surname
-    clas = message.text
-    sent = bot.send_message(message.chat.id, f'{surname.capitalize()} {name.capitalize()} {clas} верно?')
-    bot.register_next_step_handler(sent, yes_no_message)
+    if len(slov[message.chat.id]) > 2:
+        slov[message.chat.id].append(message.text)
+        sent = bot.send_message(message.chat.id, f'{slov[message.chat.id][2].capitalize()}'
+                                                 f' {slov[message.chat.id][1].capitalize()}'
+                                                 f' {slov[message.chat.id][3]} верно?')
+        print(slov)
+        bot.register_next_step_handler(sent, yes_no_message)
 
 
 def yes_no_message(message):
     if message.text.lower() == 'да':
+        a = slov.pop(message.chat.id)
         pass
     else:
-        sent = bot.send_message(message.from_user.id, 'Давай заново. Как тебя зовут')
+        a = slov.pop(message.chat.id)
+        sent = bot.send_message(message.chat.id, 'Давай заново. Как тебя зовут')
+        slov[message.chat.id] = ['start']
+        print(slov)
         bot.register_next_step_handler(sent, surname_message)
 
 
